c92ec246ff005d01a1b9a5ebffa181b408e35ef2
Jesus Vaquerizo <32816191+jvaque@users.noreply.github.com>
2021-07-29 19:18:11 +0100

    Renamed Default Form

--- multi-thread/mandelbrot/mandelbrot/MandelbrotForm.cs
+++ multi-thread/mandelbrot/mandelbrot/MandelbrotForm.cs
@@ -10,12 +10,11 @@ using System.Windows.Forms;
 
 namespace mandelbrot
 {
-    public partial class Form1 : Form
+    public partial class MandelbrotForm : Form
     {
-        public Form1()
+        public MandelbrotForm()
         {
             InitializeComponent();
         }
-
     }
 }
