3d39435143a2c866ac6e3f1851cb642f140ef9c2
Jesus Vaquerizo <32816191+jvaque@users.noreply.github.com>
2021-07-29 20:50:58 +0100

    Implement Basic Mandelbrot Single Threaded Monochrome
    Basic implementation without splitting into function, also
    singlethreaded and producing a monochrome result

--- multi-thread/mandelbrot/mandelbrot/MandelbrotForm.Designer.cs
+++ multi-thread/mandelbrot/mandelbrot/MandelbrotForm.Designer.cs
@@ -29,12 +29,34 @@ namespace mandelbrot
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBoxMandelbrot = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMandelbrot)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxMandelbrot
+            // 
+            this.pictureBoxMandelbrot.Location = new System.Drawing.Point(12, 12);
+            this.pictureBoxMandelbrot.Name = "pictureBoxMandelbrot";
+            this.pictureBoxMandelbrot.Size = new System.Drawing.Size(776, 776);
+            this.pictureBoxMandelbrot.TabIndex = 0;
+            this.pictureBoxMandelbrot.TabStop = false;
+            // 
+            // MandelbrotForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 800);
+            this.Controls.Add(this.pictureBoxMandelbrot);
+            this.Name = "MandelbrotForm";
             this.Text = "MandelbrotForm";
+            this.Shown += new System.EventHandler(this.MandelbrotForm_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMandelbrot)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxMandelbrot;
     }
 }
--- multi-thread/mandelbrot/mandelbrot/MandelbrotForm.cs
+++ multi-thread/mandelbrot/mandelbrot/MandelbrotForm.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,39 @@ namespace mandelbrot
         public MandelbrotForm()
         {
             InitializeComponent();
+        }
+
+        private void MandelbrotForm_Shown(object sender, EventArgs e)
+        {
+            Bitmap bm = new Bitmap(pictureBoxMandelbrot.Width, pictureBoxMandelbrot.Height);
+
+            for (int x = 0; x < pictureBoxMandelbrot.Width; x++)
+            {
+                for (int y = 0; y < pictureBoxMandelbrot.Height; y++)
+                {
+                    double a = (double)(x - (pictureBoxMandelbrot.Width / 2)) / (double)(pictureBoxMandelbrot.Width / 4);
+                    double b = (double)(y - (pictureBoxMandelbrot.Height / 2)) / (double)(pictureBoxMandelbrot.Height / 4);
+                    Complex c = new Complex(a, b);
+                    Complex z = new Complex(0, 0);
+
+                    int iterations = 100;
+                    int iterationsRun = 0;
+                    for (int i = 0; i < iterations; i++)
+                    {
+                        z = Complex.Pow(z, 2);
+                        z += c;
+                        if (z.Magnitude > 2)
+                        {
+                            break;
+                        }
+                        iterationsRun++;
+                    }
+
+                    bm.SetPixel(x, y, iterationsRun < 100 ? Color.Black : Color.White);
+                }
+            }
+
+            pictureBoxMandelbrot.Image = bm;
         }
     }
 }
