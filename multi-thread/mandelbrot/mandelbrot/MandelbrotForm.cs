216f5ec6740d32d450aa57c290c29763ddad9227
Jesus Vaquerizo <32816191+jvaque@users.noreply.github.com>
2021-07-30 08:20:02 +0100

    Added Zoom And Translation Of Center Point
    Added the ability to zoom and translate the center of the image, also
    moved the choosing of the colour to a separate function.
    Also fixed imaginary axis by negating it

--- multi-thread/mandelbrot/mandelbrot/MandelbrotForm.cs
+++ multi-thread/mandelbrot/mandelbrot/MandelbrotForm.cs
@@ -22,21 +22,30 @@ namespace mandelbrot
         {
             Bitmap bm = new Bitmap(pictureBoxMandelbrot.Width, pictureBoxMandelbrot.Height);
 
+            double aOffset = -0.75;
+            double bOffset = 0.015;
+            //double aOffset = -1.315180982097868;
+            //double bOffset = 0.073481649996795;
+            double zoom = 0.00001;
+            int iterations = 10000;
+
             for (int x = 0; x < pictureBoxMandelbrot.Width; x++)
             {
                 for (int y = 0; y < pictureBoxMandelbrot.Height; y++)
                 {
-                    double a = (double)(x - (pictureBoxMandelbrot.Width / 2)) / (double)(pictureBoxMandelbrot.Width / 4);
-                    double b = (double)(y - (pictureBoxMandelbrot.Height / 2)) / (double)(pictureBoxMandelbrot.Height / 4);
+                    double a = (((double)(x - (pictureBoxMandelbrot.Width / 2)) / (double)(pictureBoxMandelbrot.Width / 4)) * zoom) + aOffset;
+                    double b = -(((double)(y - (pictureBoxMandelbrot.Height / 2)) / (double)(pictureBoxMandelbrot.Height / 4)) * zoom) + bOffset;
                     Complex c = new Complex(a, b);
                     Complex z = new Complex(0, 0);
 
-                    int iterationsRun = CalculateMandelbrot(z, c, 255);
+                    int iterationsRun = CalculateMandelbrot(z, c, iterations);
 
-                    bm.SetPixel(x, y, iterationsRun < 100 ? Color.Black : Color.White);
+                    bm.SetPixel(x, y, GetColor(iterationsRun, iterations));
                 }
             }
 
+            // Place Pixel at center of screen
+            // bm.SetPixel(pictureBoxMandelbrot.Width / 2, pictureBoxMandelbrot.Height / 2, Color.White);
             pictureBoxMandelbrot.Image = bm;
         }
 
@@ -44,7 +53,7 @@ namespace mandelbrot
         {
             int iteration = 0;
 
-            while (z.Magnitude <= 2 && iteration <= maxIterations)
+            while (z.Magnitude <= 2 && iteration < maxIterations)
             {
                 iteration++;
                 z = Complex.Pow(z, 2);
@@ -52,6 +61,22 @@ namespace mandelbrot
             }
 
             return iteration;
+        }
+
+        private Color GetColor(int iterationsRun, int maxIterations)
+        {
+            if (iterationsRun == maxIterations)
+            {
+                return Color.Black;
+            }
+            else
+            {
+                double ttttttttttt = (double)(iterationsRun) / maxIterations;
+
+                int blueness = (int)(255 * ttttttttttt);
+
+                return Color.FromArgb(40, 10, blueness);
+            }
         }
     }
 }
