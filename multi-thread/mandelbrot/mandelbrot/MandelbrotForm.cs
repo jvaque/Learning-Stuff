476f734b37bd3eec8f76adfd3605bdbf9cc9fb82
Jesus Vaquerizo <32816191+jvaque@users.noreply.github.com>
2022-02-13 17:15:20 +0000

    Resize And Reposition Image For Display
    Resize and reposition the generated image to fit inside the display
    canvas area so that it's centered and maintains it's original aspect
    ratio.

--- multi-thread/mandelbrot/mandelbrot/MandelbrotForm.cs
+++ multi-thread/mandelbrot/mandelbrot/MandelbrotForm.cs
@@ -1,8 +1,9 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
@@ -15,7 +16,14 @@ namespace mandelbrot
     public partial class MandelbrotForm : Form
     {
         //private MandelbrotGenerator _mbGenerator = new(1920, 1080);
-        private MandelbrotGenerator _mbGenerator = new(3840, 2160);
+        //private MandelbrotGenerator _mbGenerator = new(1080, 1980);
+        //private MandelbrotGenerator _mbGenerator = new(960, 540);
+        //private MandelbrotGenerator _mbGenerator = new(540, 960);
+        private MandelbrotGenerator _mbGenerator = new(1000, 1000);
+        //private MandelbrotGenerator _mbGenerator = new(2000, 1000);
+        //private MandelbrotGenerator _mbGenerator = new(1000, 2000);
+        //private MandelbrotGenerator _mbGenerator = new(3840, 2160);
+        //private MandelbrotGenerator _mbGenerator = new(2160, 3840);
         //private MandelbrotGenerator _mbGenerator = new(16000,9000);
 
         int threadCount = Environment.ProcessorCount;
@@ -33,7 +41,7 @@ namespace mandelbrot
             {
                 buttonStart.Text = "Running";
 
-                pictureBoxMandelbrot.Image = _mbGenerator.Calculate();
+                pictureBoxMandelbrot.Image = BitmapResize(_mbGenerator.Calculate(), pictureBoxMandelbrot.Width, pictureBoxMandelbrot.Height);
 
                 buttonStart.Text = "Clear";
             }
@@ -43,8 +51,73 @@ namespace mandelbrot
 
                 buttonStart.Text = "Start";
             }
-            
+
             buttonStart.Enabled = true;
+        }
+
+        /// <summary>
+        /// Generate a rectangle for the image to get scaled down into the
+        ///  canvas, such that the rectangle is centered relative to the canvas
+        ///  and the rectangle maintains the original image aspect ratio.
+        /// </summary>
+        /// <param name="canvasWidth"></param>
+        /// <param name="canvasHeight"></param>
+        /// <param name="pictureWidth"></param>
+        /// <param name="pictureHeight"></param>
+        /// <returns></returns>
+        private static Rectangle CalculateRectangle(int canvasWidth, int canvasHeight, int pictureWidth, int pictureHeight)
+        {
+            // Calculate the width, height and starting points for the scaled down rectangle to fit in the canvas
+            double aspectRatioCanvas = (double)canvasWidth / (double)canvasHeight;
+            double aspectRatioPicture = (double)pictureWidth / (double)pictureHeight;
+
+            if (aspectRatioCanvas > aspectRatioPicture)
+            {
+                int newWidth = (int)(canvasHeight * aspectRatioPicture);
+                int widthStartPoint = (canvasWidth / 2) - (newWidth / 2);
+                return new Rectangle(widthStartPoint, 0, newWidth, canvasHeight);
+            }
+            else if (aspectRatioCanvas < aspectRatioPicture)
+            {
+                int newHeight = (int)(canvasWidth / aspectRatioPicture);
+                int heightStartPoint = canvasHeight / 2 - newHeight / 2;
+                return new Rectangle(0, heightStartPoint, canvasWidth, newHeight);
+            }
+            else
+            {
+                // aspectRatioCanvas == aspectRatioPicture
+                return new Rectangle(0, 0, canvasWidth, canvasHeight);
+            }
+        }
+
+        private static Bitmap BitmapResize(Bitmap image, int width, int height)
+        {
+
+            //var destRect = new Rectangle(0, 0, width, height);
+            var destRect = CalculateRectangle(width, height, image.Width, image.Height);
+            var destImage = new Bitmap(width, height);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                    // Tiling with multiples of the image.Width and image.Height result in intresting results to maybe
+                    //  explore some other time.
+                    // graphics.DrawImage(image, destRect, 0, 0, image.Width * 6, image.Height * 5, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            return destImage;
         }
 
         private void buttonSaveImage_Click(object sender, EventArgs e)
