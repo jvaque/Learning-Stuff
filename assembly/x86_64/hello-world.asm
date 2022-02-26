6d50253a8752480332cc7c241523e0349a863dd6
Jesus Vaquerizo <32816191+jvaque@users.noreply.github.com>
2022-02-26 13:17:31 +0000

    Added Comments Explainig Code In Detail
    Added some comments explaining in detail what each line in the assembly
    code does

--- assembly/x86_64/hello-world.asm
+++ assembly/x86_64/hello-world.asm
@@ -1,16 +1,19 @@
 section .data
-        text db "Hello, World!",10
+        text db "Hello, World!",10      ;Define bytes "Hello, World!\n" and
+                                        ; label the memory address as "text" 
 
 section .text
         global _start
 
 _start:
-        mov rax, 1
-        mov rdi, 1
-        mov rsi, text
-        mov rdx, 14
-        syscall                 ;system
+                                        ; sys_write(1, text, 14)
+        mov rax, 1                      ;System call ID for sys_write
+        mov rdi, 1                      ;File descriptor for Standard Output
+        mov rsi, text                   ;Location in memory for the string to write
+        mov rdx, 14                     ;Length of the string to write
+        syscall                         ;Invoke kernel system call
 
-        mov rax, 60
-        mov rdi, 0
-        syscall
+                                        ; sys_exit(0)
+        mov rax, 60                     ;System call ID for sys_exit
+        mov rdi, 0                      ;Error code value for no error
+        syscall                         ;Invoke kernel system call
