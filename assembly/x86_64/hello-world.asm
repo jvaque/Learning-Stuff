d9436556f04fcc780094371295b3cb1161b4204b
Jesus Vaquerizo <32816191+jvaque@users.noreply.github.com>
2022-02-26 13:38:43 +0000

    Added Comments Explaining The Sections
    Added some more comments explaining what the different sections in
    x86_64 are for

--- assembly/x86_64/hello-world.asm
+++ assembly/x86_64/hello-world.asm
@@ -1,10 +1,16 @@
+; data section is where data is defined before compilation
 section .data
         text db "Hello, World!",10      ;Define bytes "Hello, World!\n" and
                                         ; label the memory address as "text" 
 
+; bss section is where data is allocated for future use
+;hello-world program doesn't need a bss section
+
+; text section is where the actual code goes
 section .text
         global _start
 
+;_start is a label for a section of code
 _start:
                                         ; sys_write(1, text, 14)
         mov rax, 1                      ;System call ID for sys_write
