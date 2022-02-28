4aa752b8e5be073a3cb9f9726eed2f92ca4ede47
Jesus Vaquerizo <32816191+jvaque@users.noreply.github.com>
2022-02-28 01:29:35 +0000

    Added Subroutine To Print Strings
    Added a subroutine to be able to print null terminated strings by
    working out the string length and then calling the relevant system call,
    avoiding having to manually set the string length

--- assembly/x86_64/print-strings.asm
+++ assembly/x86_64/print-strings.asm
@@ -1,25 +1,40 @@
-; data section is where data is defined before compilation
 section .data
-        text db "Hello, World!",10      ;Define bytes "Hello, World!\n" and
-                                        ; label the memory address as "text" 
+        text db "Hello, World!",10,0    ;Define null terminated string
+        huzza db "Huzzah! Now you can print arbitrary lenth null terminated strings",10,0
 
-; bss section is where data is allocated for future use
-;hello-world program doesn't need a bss section
-
-; text section is where the actual code goes
 section .text
         global _start
 
-;_start is a label for a section of code
 _start:
-                                        ; sys_write(1, text, 14)
-        mov rax, 1                      ;System call ID for sys_write
-        mov rdi, 1                      ;File descriptor for Standard Output
-        mov rsi, text                   ;Location in memory for the string to write
-        mov rdx, 14                     ;Length of the string to write
-        syscall                         ;Invoke kernel system call
+        mov rax, text                   ;Load values needed by the subroutine
+        call _print                     ;Call subroutine
+
+        mov rax, huzza
+        call _print
 
                                         ; sys_exit(0)
         mov rax, 60                     ;System call ID for sys_exit
         mov rdi, 0                      ;Error code value for no error
         syscall                         ;Invoke kernel system call
+
+;input: rax as pointer to string
+;output: print string at rax
+_print:
+        push rax                        ;Save value of rax for later
+        mov rbx, 0                      ;Initialize value or rbx to zero to act
+                                        ; as the string length count
+_printLoop:
+        inc rax                         ;Increment rax register
+        inc rbx                         ;Increment rbx register
+        mov cl, [rax]                   ;Move into the 8 bit equivalent of rcx
+                                        ; the value pointed by rax
+        cmp cl, 0                       ;Compare cl to zero (end of the string)
+        jne _printLoop                  ;Jump if not equal to _printLoop
+
+        mov rax, 1                      ;Invoke syscall to print string
+        mov rdi, 1
+        pop rsi                         ;Pop value from the stack into rsi register
+        mov rdx, rbx
+        syscall
+
+        ret                             ;Return from subroutine
