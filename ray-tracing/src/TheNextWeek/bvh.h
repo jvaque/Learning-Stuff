d486e524eb277e182307cc44775b506cb965711f
Jesus Vaquerizo <32816191+jvaque@users.noreply.github.com>
2023-05-24 14:56:31 +0100

    Chapter 3.10: The Box Comparison Functions

--- ray-tracing/src/TheNextWeek/bvh.h
+++ ray-tracing/src/TheNextWeek/bvh.h
@@ -97,4 +97,32 @@ bvh_node::bvh_node(
     box = surrounding_box(box_left, box_right);
 }
 
+inline bool box_compare(const std::shared_ptr<hittable> a, const std::shared_ptr<hittable> b, int axis)
+{
+    aabb box_a;
+    aabb box_b;
+
+    if (!a->bounding_box(0, 0, box_a) || !b->bounding_box(0, 0, box_b))
+    {
+        std::cerr << "No bounding box in bvh_node constructor.\n";
+    }
+
+    return box_a.min().e[axis] < box_b.min().e[axis];
+}
+
+bool box_x_compare(const std::shared_ptr<hittable> a, const std::shared_ptr<hittable> b)
+{
+    return box_compare(a, b, 0);
+}
+
+bool box_y_compare(const std::shared_ptr<hittable> a, const std::shared_ptr<hittable> b)
+{
+    return box_compare(a, b, 1);
+}
+
+bool box_z_compare(const std::shared_ptr<hittable> a, const std::shared_ptr<hittable> b)
+{
+    return box_compare(a, b, 2);
+}
+
 #endif // BVH_H
