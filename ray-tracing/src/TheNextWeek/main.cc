a7b9670124e2a729800eb729e748e362a7bab9fa
Jesus Vaquerizo <32816191+jvaque@users.noreply.github.com>
2023-05-29 15:23:47 +0100

    Chapter 4.3: A Checker Texture

--- ray-tracing/src/TheNextWeek/main.cc
+++ ray-tracing/src/TheNextWeek/main.cc
@@ -39,8 +39,8 @@ hittable_list random_scene()
 {
     hittable_list world;
 
-    auto ground_material = std::make_shared<lambertian>(color(0.5, 0.5, 0.5));
-    world.add(std::make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
+    auto checker = std::make_shared<checker_texture>(color(0.2, 0.3, 0.1), color(0.9, 0.9, 0.9));
+    world.add(std::make_shared<sphere>(point3(0, -1000, 0), 1000, std::make_shared<lambertian>(checker)));
 
     for (int a = -11; a < 11; a++)
     {
--- ray-tracing/src/common/texture.h
+++ ray-tracing/src/common/texture.h
@@ -27,4 +27,33 @@ class solid_color : public texture
         color color_value;
 };
 
+class checker_texture : public texture
+{
+    public:
+        checker_texture() {}
+
+        checker_texture(std::shared_ptr<texture> _even, std::shared_ptr<texture> _odd)
+            : even(_even), odd(_odd) {}
+
+        checker_texture(color c1, color c2)
+            : even(std::make_shared<solid_color>(c1)), odd(std::make_shared<solid_color>(c2)) {}
+
+        virtual color value(double u, double v, const point3& p) const override
+        {
+            auto sines = sin(10*p.x()) * sin(10*p.y()) * sin(10*p.z());
+            if (sines < 0)
+            {
+                return odd->value(u, v, p);
+            }
+            else
+            {
+                return even->value(u, v, p);
+            }
+        }
+
+    public:
+        std::shared_ptr<texture> odd;
+        std::shared_ptr<texture> even;
+};
+
 #endif // TEXTURE_H
