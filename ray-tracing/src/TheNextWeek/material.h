2976f69bd1f25246aee74e5c35257d5be626339d
Jesus Vaquerizo <32816191+jvaque@users.noreply.github.com>
2023-06-06 16:22:57 +0100

    Chapter 7.1: Emissive Materials

--- ray-tracing/src/TheNextWeek/material.h
+++ ray-tracing/src/TheNextWeek/material.h
@@ -12,6 +12,10 @@ class material
         virtual bool scatter(
             const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
         ) const = 0;
+        virtual color emitted(double u, double v, const point3& p) const
+        {
+            return color(0, 0, 0);
+        }
 };
 
 class lambertian : public material
@@ -104,6 +108,28 @@ class dielectric : public material
             r0 = r0 * r0;
             return r0 + (1 - r0) * pow((1 - cosine), 5);
         }
+};
+
+class diffuse_light : public material
+{
+    public:
+        diffuse_light(std::shared_ptr<texture> a) : emit(a) {}
+        diffuse_light(color c) : emit(std::make_shared<solid_color>(c)) {}
+
+        virtual bool scatter(
+            const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
+        ) const override
+        {
+            return false;
+        }
+
+        virtual color emitted(double u, double v, const point3& p) const override
+        {
+            return emit->value(u, v, p);
+        }
+
+    public:
+        std::shared_ptr<texture> emit;
 };
 
 #endif // MATERIAL_H
