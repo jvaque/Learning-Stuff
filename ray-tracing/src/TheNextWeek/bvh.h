0267dc47bdd1f34636905572b9cc3898dcd9fa5c
Jesus Vaquerizo <32816191+jvaque@users.noreply.github.com>
2023-07-05 17:37:44 +0100

    Chapter 10: A Scene Testing All New Features

--- ray-tracing/src/TheNextWeek/bvh.h
+++ ray-tracing/src/TheNextWeek/bvh.h
@@ -48,8 +48,36 @@ bool bvh_node::hit(const ray& r, double t_min, double t_max, hit_record& rec) co
     return hit_left || hit_right;
 }
 
+inline bool box_compare(const std::shared_ptr<hittable> a, const std::shared_ptr<hittable> b, int axis)
+{
+    aabb box_a;
+    aabb box_b;
+
+    if (!a->bounding_box(0, 0, box_a) || !b->bounding_box(0, 0, box_b))
+    {
+        std::cerr << "No bounding box in bvh_node constructor.\n";
+    }
+
+    return box_a.min().e[axis] < box_b.min().e[axis];
+}
+
+bool box_x_compare(const std::shared_ptr<hittable> a, const std::shared_ptr<hittable> b)
+{
+    return box_compare(a, b, 0);
+}
+
+bool box_y_compare(const std::shared_ptr<hittable> a, const std::shared_ptr<hittable> b)
+{
+    return box_compare(a, b, 1);
+}
+
+bool box_z_compare(const std::shared_ptr<hittable> a, const std::shared_ptr<hittable> b)
+{
+    return box_compare(a, b, 2);
+}
+
 bvh_node::bvh_node(
-    std::vector<std::shared_ptr<hittable>>& src_objects,
+    const std::vector<std::shared_ptr<hittable>>& src_objects,
     size_t start, size_t end, double time0, double time1)
 {
     auto objects = src_objects; // Create a modifiable array of the source scene objects
@@ -95,34 +123,6 @@ bvh_node::bvh_node(
     }
 
     box = surrounding_box(box_left, box_right);
-}
-
-inline bool box_compare(const std::shared_ptr<hittable> a, const std::shared_ptr<hittable> b, int axis)
-{
-    aabb box_a;
-    aabb box_b;
-
-    if (!a->bounding_box(0, 0, box_a) || !b->bounding_box(0, 0, box_b))
-    {
-        std::cerr << "No bounding box in bvh_node constructor.\n";
-    }
-
-    return box_a.min().e[axis] < box_b.min().e[axis];
-}
-
-bool box_x_compare(const std::shared_ptr<hittable> a, const std::shared_ptr<hittable> b)
-{
-    return box_compare(a, b, 0);
-}
-
-bool box_y_compare(const std::shared_ptr<hittable> a, const std::shared_ptr<hittable> b)
-{
-    return box_compare(a, b, 1);
-}
-
-bool box_z_compare(const std::shared_ptr<hittable> a, const std::shared_ptr<hittable> b)
-{
-    return box_compare(a, b, 2);
 }
 
 #endif // BVH_H
