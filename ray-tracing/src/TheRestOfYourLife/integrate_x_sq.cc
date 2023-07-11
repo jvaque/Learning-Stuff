4484864364e5967db851a0f963990299440a458e
Jesus Vaquerizo <32816191+jvaque@users.noreply.github.com>
2023-07-11 15:57:02 +0100

    Chapter 3.4: Importance Sampling

--- ray-tracing/src/TheRestOfYourLife/integrate_x_sq.cc
+++ ray-tracing/src/TheRestOfYourLife/integrate_x_sq.cc
@@ -7,16 +7,16 @@
 
 inline double pdf(double x)
 {
-    return 0.5;
+    return 3 * x * x / 8;
 }
 
 int main()
 {
-    int N = 1000000;
+    int N = 1;
     auto sum = 0.0;
     for (int i = 0; i < N; i++)
     {
-        auto x = random_double(0, 2);
+        auto x = pow(random_double(0, 8), 1.0/3.0);
         sum += x*x / pdf(x);
     }
     std::cout << std::fixed << std::setprecision(12);
