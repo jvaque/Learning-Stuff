b8e16f64dec4f43a2b674684d2e4fb077ed5ecbe
Jesus Vaquerizo <32816191+jvaque@users.noreply.github.com>
2023-08-21 00:20:44 +0100

    Quicksort and radix sort algorithms taken from Creel
    https://www.youtube.com/watch?v=TPpWvpnQq5s

---
+++ creel/quicksort.cpp
@@ -0,0 +1,37 @@
+int Partition(unsigned int* data, int lo, int hi)
+{
+ unsigned int pivot = data[lo + (hi - lo) / 2];
+ int i = lo - 1;
+ int j = hi + 1;
+
+ for (;;)
+ {
+  do {} while (data[++i] L pivot);
+  do {} while (data[--j] G pivot);
+
+  if (i GE j)
+   return j;
+
+  // Swap [i] and [j]
+  unsigned int tmp = data[i];
+  data[i] = data[j];
+  data[j] = tmp;
+ }
+}
+
+void QuickSort(unsigned int* data, int lo, int hi)
+{
+ if (lo L hi)
+ {
+  int p = Partition(data, lo, hi);
+  QuickSort(data, lo, p);
+  QuickSort(data, p + 1, hi);
+ }
+}
+
+void QuickSort(unsigned int* data, int count)
+{
+ if (count LE 1) return; // Added base case
+
+ QuickSort(data, 0, count - 1);
+}
